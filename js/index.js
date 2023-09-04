b0ef9b3338113847a0546a5cb80ca874ba655af4
Xinru Lu <865666015@qq.com>
2023-09-05 00:01:07 +0800

    fixed bug

--- js/index.js
+++ js/index.js
@@ -47,6 +47,13 @@ window.onload = function () {
    */
   const chart6 = document.getElementById("chart6");
   getChart6Data(chart6);
+  /**
+   * 产量运行
+   */
+  const chart4 = document.getElementById("chart4");
+  getChart4Data(chart4);
+  const chart7 = document.getElementById("chart7");
+  getChart7Data(chart7);
 };
 
 function formatTime(date) {
@@ -153,10 +160,10 @@ function getChart1Data(dom) {
           show: false,
         },
         splitLine: {
-          show: true,
-          lineStyle: {
-            color: "rgba(255, 207, 144, 1)",
-          },
+          show: false,
+          // lineStyle: {
+          //   color: "rgba(255, 207, 144, 1)",
+          // },
         },
         axisLabel: {
           fontSize: 14,
@@ -170,6 +177,7 @@ function getChart1Data(dom) {
         yAxisIndex: 0,
         type: "bar",
         barGap: 0,
+        barWidth: 15,
         emphasis: {
           focus: "series",
         },
@@ -203,6 +211,7 @@ function getChart1Data(dom) {
           focus: "series",
         },
         data: [220, 182, 191, 234, 290, 320, 332, 301, 334, 390, 320, 332],
+        barWidth: 15,
         itemStyle: {
           color: {
             type: "linear",
@@ -798,378 +807,165 @@ function getChart5Data(dom) {
   // 基于准备好的dom，初始化echarts实例
   const myChart = echarts.init(dom);
 
-  yList = [32];
-  yList2 = [52];
-  xList = [10];
-  color = [
-    "#CC1CAA",
-    "#8D67FF",
-    "#00FFFF",
-    "#48DE13",
-    "#FFC516",
-    "#DC3E14",
-    "#8E16F8",
-  ];
-  xData = xList.map((item, index) => {
-    return {
-      value: item,
-      textStyle: {
-        color: color[index],
-      },
-    };
-  });
-  dom = 800;
-  barWidth = dom / 20;
-  colors = [
-    {
-      type: "linear",
-      x: 0,
-      x2: 1,
-      y: 0,
-      y2: 0,
-      colorStops: [
-        {
-          offset: 0,
-          color: "#FF3FDA",
-        },
-        {
-          offset: 0.5,
-          color: "#FF3FDA",
-        },
-        {
-          offset: 0.5,
-          color: "#CC1BAA",
-        },
-        {
-          offset: 1,
-          color: "#CC1BAA",
-        },
-      ],
-    },
-    {
-      type: "linear",
-      x: 0,
-      x2: 1,
-      y: 0,
-      y2: 0,
-      colorStops: [
-        {
-          offset: 0,
-          color: "#602CFF",
-        },
-        {
-          offset: 0.5,
-          color: "#602CFF",
-        },
-        {
-          offset: 0.5,
-          color: "#4915E6",
-        },
-        {
-          offset: 1,
-          color: "#4915E6",
-        },
-      ],
-    },
-    {
-      type: "linear",
-      x: 0,
-      x2: 1,
-      y: 0,
-      y2: 0,
-      colorStops: [
-        {
-          offset: 0,
-          color: "#00FFFF",
-        },
-        {
-          offset: 0.5,
-          color: "#00FFFF",
-        },
-        {
-          offset: 0.5,
-          color: "#11A6D0",
-        },
-        {
-          offset: 1,
-          color: "#11A6D0",
-        },
-      ],
-    },
-    {
-      type: "linear",
-      x: 0,
-      x2: 1,
-      y: 0,
-      y2: 0,
-      colorStops: [
-        {
-          offset: 0,
-          color: "#89FF5E",
-        },
-        {
-          offset: 0.5,
-          color: "#89FF5E",
-        },
-        {
-          offset: 0.5,
-          color: "#48DD12",
-        },
-        {
-          offset: 1,
-          color: "#48DD12",
-        },
-      ],
-    },
-    {
-      type: "linear",
-      x: 0,
-      x2: 1,
-      y: 0,
-      y2: 0,
-      colorStops: [
-        {
-          offset: 0,
-          color: "#FFD05C",
-        },
-        {
-          offset: 0.5,
-          color: "#FFD05C",
-        },
-        {
-          offset: 0.5,
-          color: "#DEA821",
-        },
-        {
-          offset: 1,
-          color: "#DEA821",
-        },
-      ],
-    },
-    {
-      type: "linear",
-      x: 0,
-      x2: 1,
-      y: 0,
-      y2: 0,
-      colorStops: [
-        {
-          offset: 0,
-          color: "#FF7853",
-        },
-        {
-          offset: 0.5,
-          color: "#FF7853",
-        },
-        {
-          offset: 0.5,
-          color: "#DB3E13",
-        },
-        {
-          offset: 1,
-          color: "#DB3E13",
-        },
-      ],
-    },
-    {
-      type: "linear",
-      x: 0,
-      x2: 1,
-      y: 0,
-      y2: 0,
-      colorStops: [
-        {
-          offset: 0,
-          color: "#AA48FF",
-        },
-        {
-          offset: 0.5,
-          color: "#AA48FF",
-        },
-        {
-          offset: 0.5,
-          color: "#8E15F8",
-        },
-        {
-          offset: 1,
-          color: "#8E15F8",
-        },
-      ],
-    },
-  ];
-
   // 指定图表的配置项和数据
   const option = {
-    //提示框
     tooltip: {
-      show: false,
+      trigger: "axis",
     },
-    /**区域位置*/
-    //   grid: {
-    //       left: '10%',
-    //       right: '10%',
-    //       top: 80,
-    //       bottom: 40,
-    //     },
-    /**图例大小*/
-    //  legend: {
-    //   type: 'plain',
-    //   left: 0,
-    //   top: 20,
-    //   itemGap: 20,
-    //   itemWidth: 35,
-    //   itemHeight: 20
-    // },
-    //X轴
-    xAxis: {
-      show: false,
-      name: "X",
-      nameTextStyle: {
-        color: "#FFFFFF",
-        padding: [0, 0, 0, 20],
-      },
-      data: xData,
-      type: "category",
-      axisLine: {
-        show: true,
-        lineStyle: {
-          color: "rgba(255,255,255,1)",
-          shadowColor: "rgba(255,255,255,1)",
-          shadowOffsetX: "20",
-        },
-        symbol: ["none", "arrow"],
-        symbolOffset: [0, 25],
-      },
-      splitLine: {
-        show: true,
-        lineStyle: {
-          color: "rgba(255,255,255,0.2)",
-        },
-      },
-      axisLabel: {
-        rotate: -30,
-        fontSize: 12,
+    legend: {
+      top: "5%",
+      left: "3%",
+      itemWidth: 6,
+      itemHeight: 6,
+      data: ["五星", "四星", "三星"],
+      itemStyle: {
+        borderWidth: 0,
       },
     },
-    yAxis: {
-      show: false,
-      splitNumber: 4,
-      axisLine: {
-        show: true,
-        lineStyle: {
-          color: "rgba(255,255,255,0.2)",
-        },
-      },
-      splitLine: {
-        show: true,
-        lineStyle: {
-          color: "rgba(255,255,255,0.2)",
-        },
-      },
-      axisLabel: {
-        color: "#FFFFFF",
-      },
+    grid: {
+      top: "20%",
+      left: "3%",
+      right: "3%",
+      bottom: "3%",
+      containLabel: true,
     },
+    xAxis: [
+      {
+        type: "category",
+        axisTick: { show: false },
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: "rgba(255, 207, 144, 1)",
+          },
+        },
+        axisLabel: {
+          fontSize: 14,
+          color: "rgba(196, 101, 85, 1)",
+        },
+        data: ["2024", "2025", "2026"],
+      },
+    ],
+    yAxis: [
+      {
+        type: "value",
+        axisLine: {
+          show: false,
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: "rgba(255, 207, 144, 1)",
+          },
+        },
+        axisLabel: {
+          fontSize: 14,
+          color: "rgba(196, 101, 85, 1)",
+        },
+      },
+      {
+        type: "value",
+        axisLine: {
+          show: false,
+        },
+        splitLine: {
+          show: false,
+          // lineStyle: {
+          //   color: "rgba(255, 207, 144, 1)",
+          // },
+        },
+        axisLabel: {
+          fontSize: 14,
+          color: "rgba(196, 101, 85, 1)",
+        },
+      },
+    ],
     series: [
       {
+        name: "三星",
+        stack: true,
         type: "bar",
-        stack: "ttt",
-        barWidth: barWidth,
+        data: [1, 2, 1],
         itemStyle: {
-          normal: {
-            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          color: {
+            type: "linear",
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
               {
                 offset: 0,
-                color: "#2450F0",
+                color: "rgba(114, 157, 53, 1)", // 0% 处的颜色
               },
               {
                 offset: 1,
-                color: "#2FCAFD",
+                color: "rgba(114, 157, 53, 1)", // 100% 处的颜色
               },
-            ]),
+            ],
+            global: false, // 缺省为 false
           },
+          shadowBlur: 20,
+          shadowColor: "rgba(114, 157, 53, 1)",
         },
-        data: yList,
       },
       {
+        name: "四星",
+        stack: true,
         type: "bar",
-        stack: "ttt",
-        barWidth: barWidth,
+        data: [3, 4, 4],
         itemStyle: {
-          normal: {
-            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          color: {
+            type: "linear",
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
               {
                 offset: 0,
-                color: "#1A4DC0",
+                color: "rgba(253, 201, 38, 1)", // 0% 处的颜色
               },
               {
                 offset: 1,
-                color: "#9E2FFD",
+                color: "rgba(255, 213, 81, 1)", // 100% 处的颜色
               },
-            ]),
+            ],
+            global: false, // 缺省为 false
           },
-        },
-        data: yList2,
-      },
-      {
-        z: 2,
-        type: "pictorialBar",
-        data: yList,
-        symbol: "diamond",
-        symbolOffset: [0, "50%"],
-        symbolSize: [barWidth, barWidth * 0.5],
-        itemStyle: {
-          normal: {
-            color: "#2FCAFD",
-          },
+          shadowBlur: 20,
+          shadowColor: "rgba(253, 201, 38, 1)",
         },
       },
       {
-        z: 3,
-        type: "pictorialBar",
-        symbolPosition: "end",
-        data: yList,
-        symbol: "diamond",
-        symbolOffset: [0, "-50%"],
-        symbolSize: [barWidth, barWidth * 0.5],
+        name: "五星",
+        type: "bar",
+        stack: true,
+        emphasis: {
+          focus: "series",
+        },
+        data: [3, 4, 7],
         itemStyle: {
-          normal: {
-            borderWidth: 0,
-            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          color: {
+            type: "linear",
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
               {
                 offset: 0,
-                color: "#3FA1FB",
+                color: "rgba(254, 138, 37, 1)", // 0% 处的颜色
               },
               {
                 offset: 1,
-                color: "#41B4F8",
+                color: "rgba(255, 158, 74, 1)", // 100% 处的颜色
               },
-            ]),
+            ],
+            global: false, // 缺省为 false
           },
-        },
-      },
-      {
-        z: 3,
-        type: "pictorialBar",
-        symbolPosition: "end",
-        data: [84],
-        symbol: "diamond",
-        symbolOffset: [0, "-50%"],
-        symbolSize: [barWidth, barWidth * 0.5],
-        itemStyle: {
-          normal: {
-            borderWidth: 0,
-            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-              {
-                offset: 0,
-                color: "#886DF8",
-              },
-              {
-                offset: 1,
-                color: "#4781F0",
-              },
-            ]),
-          },
+          shadowBlur: 20,
+          shadowColor: "rgba(254, 138, 37, 1)",
         },
       },
     ],
@@ -1186,7 +982,7 @@ function getChart6Data(dom) {
   // 指定图表的配置项和数据
   const option = {
     tooltip: {
-      trigger: "axis",
+      trigger: "shadow",
     },
     legend: {
       top: "5%",
@@ -1241,12 +1037,7 @@ function getChart6Data(dom) {
     series: [
       {
         name: "一星党支部",
-        yAxisIndex: 0,
         type: "bar",
-        barGap: 0,
-        emphasis: {
-          focus: "series",
-        },
         data: [5],
         itemStyle: {
           color: {
@@ -1267,6 +1058,8 @@ function getChart6Data(dom) {
             ],
             global: false, // 缺省为 false
           },
+          shadowBlur: 20,
+          shadowColor: "rgba(0, 0, 0, 0.5)",
         },
         label: {
           show: true,
@@ -1290,11 +1083,7 @@ function getChart6Data(dom) {
       },
       {
         name: "二星党支部",
-        yAxisIndex: 0,
         type: "bar",
-        emphasis: {
-          focus: "series",
-        },
         data: [9],
         itemStyle: {
           color: {
@@ -1315,6 +1104,8 @@ function getChart6Data(dom) {
             ],
             global: false, // 缺省为 false
           },
+          shadowBlur: 20,
+          shadowColor: "rgba(0, 0, 0, 0.5)",
         },
         label: {
           show: true,
@@ -1338,11 +1129,7 @@ function getChart6Data(dom) {
       },
       {
         name: "三星党支部",
-        yAxisIndex: 0,
         type: "bar",
-        emphasis: {
-          focus: "series",
-        },
         data: [15],
         itemStyle: {
           color: {
@@ -1363,6 +1150,8 @@ function getChart6Data(dom) {
             ],
             global: false, // 缺省为 false
           },
+          shadowBlur: 20,
+          shadowColor: "rgba(0, 0, 0, 0.5)",
         },
         label: {
           show: true,
@@ -1386,11 +1175,7 @@ function getChart6Data(dom) {
       },
       {
         name: "四星党支部",
-        yAxisIndex: 0,
         type: "bar",
-        emphasis: {
-          focus: "series",
-        },
         data: [7],
         itemStyle: {
           color: {
@@ -1411,6 +1196,8 @@ function getChart6Data(dom) {
             ],
             global: false, // 缺省为 false
           },
+          shadowBlur: 20,
+          shadowColor: "rgba(0, 0, 0, 0.5)",
         },
         label: {
           show: true,
@@ -1434,11 +1221,7 @@ function getChart6Data(dom) {
       },
       {
         name: "五星党支部",
-        yAxisIndex: 0,
         type: "bar",
-        emphasis: {
-          focus: "series",
-        },
         data: [6],
         itemStyle: {
           color: {
@@ -1459,6 +1242,8 @@ function getChart6Data(dom) {
             ],
             global: false, // 缺省为 false
           },
+          shadowBlur: 20,
+          shadowColor: "rgba(0, 0, 0, 0.5)",
         },
         label: {
           show: true,
@@ -1479,6 +1264,232 @@ function getChart6Data(dom) {
             },
           },
         },
+      },
+    ],
+  };
+
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
+}
+
+function getChart4Data(dom) {
+  // 基于准备好的dom，初始化echarts实例
+  const myChart = echarts.init(dom);
+
+  // 指定图表的配置项和数据
+  const option = {
+    tooltip: {
+      trigger: "axis",
+    },
+    legend: {
+      top: "5%",
+      left: "3%",
+      itemWidth: 6,
+      itemHeight: 6,
+      data: ["原油产量", "天然气产量"],
+      icon: "rect",
+    },
+    grid: {
+      top: "20%",
+      left: "3%",
+      right: "3%",
+      bottom: "3%",
+      containLabel: true,
+    },
+    xAxis: [
+      {
+        type: "category",
+        axisTick: { show: false },
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: "rgba(255, 207, 144, 1)",
+          },
+        },
+        axisLabel: {
+          fontSize: 14,
+          color: "rgba(196, 101, 85, 1)",
+        },
+        data: [
+          "01",
+          "02",
+          "03",
+          "04",
+          "05",
+          "06",
+          "07",
+          "08",
+          "09",
+          "10",
+          "11",
+          "12",
+        ],
+      },
+    ],
+    yAxis: [
+      {
+        type: "value",
+        axisLine: {
+          show: false,
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: "rgba(255, 220, 174, 1)",
+          },
+        },
+        axisLabel: {
+          fontSize: 14,
+          color: "rgba(196, 101, 85, 1)",
+        },
+      },
+    ],
+    series: [
+      {
+        name: "原油产量",
+        type: "line",
+        showSymbol: false,
+        smooth: true,
+        data: [320, 332, 301, 334, 390, 320, 332, 301, 334, 390, 320, 332],
+        lineStyle: {
+          color: "rgba(255, 56, 56, 1)",
+        },
+        itemStyle: {
+          color: "rgba(255, 56, 56, 1)",
+        },
+        areaStyle: {
+          color: {
+            type: "linear",
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
+              {
+                offset: 0,
+                color: "rgba(255, 56, 56, 0.3)", // 0% 处的颜色
+              },
+              {
+                offset: 1,
+                color: "rgba(255, 56, 56, 0)", // 100% 处的颜色
+              },
+            ],
+          },
+        },
+      },
+      {
+        name: "天然气产量",
+        type: "line",
+        showSymbol: false,
+        smooth: true,
+        data: [220, 182, 191, 234, 290, 320, 332, 220, 182, 191, 234, 290],
+        lineStyle: {
+          color: "rgba(255, 175, 100, 1)",
+        },
+        itemStyle: {
+          color: "rgba(255, 175, 100, 1)",
+        },
+        areaStyle: {
+          color: {
+            type: "linear",
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
+              {
+                offset: 0,
+                color: "rgba(255, 175, 100, 0.3)", // 0% 处的颜色
+              },
+              {
+                offset: 1,
+                color: "rgba(255, 175, 100, 0)", // 100% 处的颜色
+              },
+            ],
+          },
+        },
+      },
+    ],
+  };
+
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
+}
+
+function getChart7Data(dom) {
+  // 基于准备好的dom，初始化echarts实例
+  const myChart = echarts.init(dom);
+
+  let colorList = [
+    "rgba(87, 183, 255, 1)",
+    "rgba(255, 186, 122, 1)",
+    "rgba(255, 242, 0, 1)",
+    "rgba(255, 56, 56, 1)",
+  ];
+  let data = [
+    { name: "SEC储量", value: 61 },
+    { name: "SEC储量", value: 30 },
+    { name: "SEC储量", value: 40 },
+    { name: "SEC储量", value: 55 },
+  ];
+  // data排序
+  data = data.sort((a, b) => {
+    return a.value - b.value;
+  });
+  // data样式
+  data.forEach((item, index) => {
+    const transparentColor = colorList[index];
+    item.itemStyle = {
+      color: transparentColor,
+      borderColor: "#fff",
+    };
+  });
+
+  // 指定图表的配置项和数据
+  const option = {
+    grid: {
+      top: "0",
+      left: "0",
+      right: "0",
+      bottom: "0",
+    },
+    series: [
+      {
+        stack: "a",
+        type: "pie",
+        radius: ["0%", "68%"],
+        center: ["50%", "50%"],
+        clockwise: true, // 鼠标悬浮效果
+        roseType: "area",
+        zlevel: 10,
+        label: {
+          show: true,
+          color: "transparent",
+          fontSize: 14,
+        },
+        labelLine: {
+          length2: 0,
+          lineStyle: {
+            color: "#fff",
+          },
+        },
+        itemStyle: {
+          borderWidth: 1,
+        },
+        data: data,
+      },
+      {
+        type: "pie",
+        color: "#fff",
+        radius: ["0%", "5%"],
+        center: ["50%", "50%"],
+        label: {
+          show: false,
+        },
+        labelLine: {
+          show: false,
+        },
+        data: [0],
       },
     ],
   };
